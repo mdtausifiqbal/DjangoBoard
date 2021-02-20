d84eab3c12d1af62b2688e1298b364d4b2d954f1
mdtausifiqbal <mdtausifiqbalnwd@gmail.com>
2021-02-20 14:47:36 +0530

    added signup view and some test

---
+++ accounts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+from django.contrib.auth import login as auth_login
+
+def signup(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('home')
+    return render(request, 'signup.html', {'form':form})
--- myproject/urls.py
+++ myproject/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from boards import views
+from accounts import views as accounts_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
     path('boards/<int:pk>/new/', views.new_topic, name="new_topic"),
     path('boards/<int:pk>', views.board_topics, name="board_topics"),
+    path('signup/', accounts_views.signup, name='signup'),
 ]
